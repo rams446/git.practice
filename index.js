ea43be21db7c4b21a3cf82fcc8c4d0995f332e2d
ramya <ramyavijaya3@gmail.com>
2023-03-07 10:51:01 -0600

    Just practicing

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Practicing my git commands");
+
+// git init
+//Folder has .git your in git repo or ls -a (command)
+//rm -rf .git (removes the folder from the git )
+//Adding or stagging is same (untracked files we are adding to commit )
+//git status
+//git add .(if you are in parent it will add all the files in current folder or subfolder)
+//. resembles current folder 
+//git add -A (no matter where you are repo it add all)
+//git add filename (it add only that file )
+//we should not init the repo inside another repo 
+//git commit -local repo 
+//git push -remote repo (git hub)
